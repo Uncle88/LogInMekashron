72e3eacc865e2dcac38985de48bd2916bbb38970
Uncle88 <andreyproskura88@gmail.com>
2017-01-11 12:58:09 +0200

    UI is ready

---
+++ LogInMekashron/LogInMekashron/Views/MyUI.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+namespace LogInMekashron.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyUI : ContentPage
+    {
+        public MyUI()
+        {
+            InitializeComponent();
+            //BindingContext = new MyUI();
+        }
+    }
+}
