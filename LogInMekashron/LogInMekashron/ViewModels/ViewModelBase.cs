11b47c53a42c968c4c7283f216ff64d37cc6e106
Uncle88 <andreyproskura88@gmail.com>
2017-01-11 18:27:20 +0200

    CreatedViewModelBase

---
+++ LogInMekashron/LogInMekashron/ViewModels/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace LogInMekashron.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
