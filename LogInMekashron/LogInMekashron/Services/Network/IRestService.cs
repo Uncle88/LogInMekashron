7b179f8f488312a825a11a4f8679315c81acd6cc
Uncle88 <andreyproskura88@gmail.com>
2017-01-13 16:37:16 +0200

    Created interface Network/IRestService

---
+++ LogInMekashron/LogInMekashron/Services/Network/IRestService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LogInMekashron.Network
+{
+    interface IRestService
+    {
+        Task<T> GetAsync<T>(string url);
+    }
+}
+
