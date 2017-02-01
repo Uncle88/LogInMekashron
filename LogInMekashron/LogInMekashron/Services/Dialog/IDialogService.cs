647bd0030154fcba0cf7c0a3965e5eebd6a6c8a2
Uncle88 <andreyproskura88@gmail.com>
2017-02-01 19:54:46 +0200

    Create an interface IDialogService from popUp message

---
+++ LogInMekashron/LogInMekashron/Services/Dialog/IDialogService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace LogInMekashron.Dialog
+{
+    public class IDialogService : ContentPage
+    {
+        public IDialogService()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
