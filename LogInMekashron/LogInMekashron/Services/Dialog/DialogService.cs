78dfb736aeae3707947e266f2daffc43b3588c76
Uncle88 <andreyproskura88@gmail.com>
2017-02-15 20:58:27 +0200

    Modify the functionality of the class DialogService - the first result is obtained

--- LogInMekashron/LogInMekashron/Services/Dialog/DialogService.cs
+++ LogInMekashron/LogInMekashron/Services/Dialog/DialogService.cs
@@ -11,22 +11,12 @@ namespace LogInMekashron.Dialog
 {
     public class DialogService : IDialogService
     {
-        LoginView _loginView;
-
-        //public void ShowMessage1(string Message)
-        //{
-        //    _loginView = new LoginView();
-        //    _loginView.DisplayAlert("Message", Message, "OK");
-        //}
-
         public void ShowMessage(string Message)
         {
-            _loginView = new LoginView();
-            Device.BeginInvokeOnMainThread(() =>
+            Device.BeginInvokeOnMainThread(async () =>
             {
-                _loginView.DisplayAlert("title", Message, "OK");
+                await Application.Current.MainPage.DisplayAlert("Message", Message, "OK");
             });
-
         }
     }
 }
