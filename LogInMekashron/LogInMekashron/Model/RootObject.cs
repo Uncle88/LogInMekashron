87a5a0c74b4fca474f045ee2e955748597af4d67
Uncle88 <andreyproskura88@gmail.com>
2017-02-20 14:05:07 +0200

    Created Model for progect

---
+++ LogInMekashron/LogInMekashron/Model/RootObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LogInMekashron.Model
+{
+    public class RootObject
+    {
+        public RootObject()
+
+        int ResultCode { get; set; }
+        string ResultMessage { get; set; }
+    }
+}
+
