c38d8ec76f4e51116deb0f4c4d2968f0ab289106
Uncle88 <andreyproskura88@gmail.com>
2017-03-24 16:07:55 +0200

    Created classes CustomLabelRenderer on iOS&Droid platform and registration with attribute

---
+++ LogInMekashron/Droid/CustomLabelRenderer.cs
@@ -0,0 +1,16 @@
+﻿using LogInMekashron;
+using LogInMekashron.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(CustomLabel), typeof(CustomLabelRenderer))]
+namespace LogInMekashron.Droid
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        public CustomLabelRenderer()
+        {
+        }
+    }
+}
---
+++ LogInMekashron/iOS/CustomLabelRenderer.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using LogInMekashron;
+using LogInMekashron.iOS;
+
+[assembly: ExportRenderer(typeof(CustomLabel), typeof(CustomLabelRenderer))]
+namespace LogInMekashron.iOS
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        public CustomLabelRenderer()
+        {
+        }
+    }
+}
