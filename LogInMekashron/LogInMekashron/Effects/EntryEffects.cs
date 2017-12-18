ce60941566ad830f8ad8a38af0a9e7532330ecb4
Andrey <andreyproskura88@gmail.com>
2017-12-19 01:52:39 +0200

    changed csproj file

--- LogInMekashron/LogInMekashron/Effects/EntryEffects.cs
+++ LogInMekashron/LogInMekashron/Effects/EntryEffects.cs
@@ -5,13 +5,7 @@ namespace LogInMekashron.Effects
 {
     public class EntryEffects : RoutingEffect
     {
-<<<<<<< HEAD
         public EntryEffects() : base("LogInMekashron.Effects.EntryEffects")
-=======
-        public Color BackGround { get; set; }
-
-        public EntryEffects() : base("LogInMekashron.EntryEffect")
->>>>>>> ba6c9e4e74c7df64aa93717e262ced1902235f84
         {
         }
     }
