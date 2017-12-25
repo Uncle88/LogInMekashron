977be2cb92b9aeba66d6211d3192ceb24cadaeb2
Andrey <andreyproskura88@gmail.com>
2017-12-25 23:34:44 +0200

    work with notification

--- LogInMekashron/LogInMekashron/ViewModels/LoginViewModel.cs
+++ LogInMekashron/LogInMekashron/ViewModels/LoginViewModel.cs
@@ -69,6 +69,10 @@ namespace LogInMekashron.ViewModels
 
                         return;
                     }
+                    else
+                    {
+                        IsBusy = false;
+                    }
                     await GetData();
                 }));
             }
